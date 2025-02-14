46bde48b678309d2b90c2d01f301a388e628ebd8
Chris Najman <christopher.najman@gmail.com>
2025-02-14 16:40:47 +0000

    Deleted popstate.js module and removed reference to it in index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-import popstate from "./js-modules/popstate.js"
 import themeSwitcher from "./js-modules/theme.js"
 import switchTshirtColour from "./js-modules/switch-tshirt-colour.js"
 import orderTshirt from "./js-modules/order-tshirt.js"
 import checkoutTshirt from "./js-modules/checkout-tshirt.js"
 
-popstate()
 themeSwitcher()
 switchTshirtColour()
 orderTshirt()
